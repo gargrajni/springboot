7ed7fa71f41f375a2bf8e549fb50692957013ec2
home <home@192.168.1.5>
2017-12-02 18:21:17 +0530

    added print statement

--- springboot/src/main/java/practice/springboot/App.java
+++ springboot/src/main/java/practice/springboot/App.java
@@ -12,7 +12,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class App 
 {
     public static void main( String[] args )
-    {
+    {	
+    	System.out.println("Hi");
     	SpringApplication.run(App.class, args);
 
     }
